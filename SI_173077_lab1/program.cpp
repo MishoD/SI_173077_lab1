6541e38bcc1ce784898bf268281f6f5b8c5cf868
unknown <Misho.Damjanovski@students.finki.ukim.mk>
2019-05-22 15:36:43 +0200

    lab2 commit

---
+++ SI_173077_lab1/program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void printMyName () {
+	cout << "Misho Damjanovski"<<endl;
+}
+
+void printMyIndex () {
+	cout<<"173077"<<endl;
+}
+
+int main () {
+	printMyName();
+	printMyIndex();
+}
